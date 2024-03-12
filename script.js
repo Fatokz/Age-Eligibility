221fa62f805479785f727a22eb53c1ca086df3d6
FATOKUN EMMANUEL <fatokunemmanuel16@gmail.com>
2024-03-13 00:02:35 +0100

    script

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 const age = 18;
+function submit() {
+    let yourAge = document.getElementById("age").value;
+    if (yourAge >= age) {
+        alert("You are eligible")
+    } else {
+        alert("You are young")
+    }
+}
